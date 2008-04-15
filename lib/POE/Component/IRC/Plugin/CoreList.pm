4d57ba727bd95965574369b86186149e6a67bdcd
Chris Williams <chris@bingosnet.co.uk>
2008-04-15 11:41:32 +0000

    Import of BINGOS/POE-Component-IRC-Plugin-CoreList-0.04 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-CoreList
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         BINGOS/POE-Component-IRC-Plugin-CoreList-0.04.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC/Plugin/CoreList.pm
+++ lib/POE/Component/IRC/Plugin/CoreList.pm
@@ -6,7 +6,7 @@ use Module::CoreList;
 use POE::Component::IRC::Plugin qw(:ALL);
 use vars qw($VERSION);
 
-$VERSION = '0.02';
+$VERSION = '0.04';
 
 my $cmds  = qr/find|search|release|date/;
 
@@ -211,6 +211,10 @@ The search never returns more than 9 replies, to avoid flooding the channel:
 Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
 
 Philippe "BooK" Bruhat, C<< <book@cpan.org> >>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
